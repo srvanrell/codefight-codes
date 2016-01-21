f6ea46dd01c437d7bb9057e7120ba5d6ab2f713d
Sebastián <srvanrell@gmail.com>
2016-01-21 00:55:47 -0300

    Work in progress

--- reversepolish.py
+++ reversepolish.py
@@ -1,15 +1,18 @@
 def ReversePolish(exp):
-    if len(exp) == 1:
-       return exp[0]
-    if len(exp) == 3:
+    print repr(exp)
+    
+    if len(exp) == 1 and isinstance(exp[0], basestring):
+#       print exp[0]
+       return exp
+    elif len(exp) == 1:
+#       print exp[0]
+       return ReversePolish(exp[0])
+    elif len(exp) == 3:
         if exp[0] == "(" and exp[2] == ")":
-            return [exp[1]]
+            return ReversePolish(exp[1])
         else:
-            return [exp[0], exp[2], exp[1]]
-    else:
-       #bl = [i for i, s in enumerate(exp) if s == "("]
-       #br = [i for i, s in enumerate(exp) if s == ")"]
-       
+            return ' '.join([ReversePolish(exp[0]), ReversePolish(exp[2]), exp[1]])
+    else:       
        if "(" in exp:
            numbrac = -1
            posini = exp.index("(")
@@ -21,14 +24,18 @@ def ReversePolish(exp):
                        posend = i
                        break
                    numbrac -= 1
-       return [exp[:posini], ReversePolish(exp[posini+1:posend]), exp[posend+1:]]
+           print "to reverse", exp[:posini] + exp[posini+1:posend] + exp[posend+1:]
+           #print exp[:posini] + ReversePolish(exp[posini+1:posend]) + exp[posend+1:]
+           return ReversePolish(exp[:posini] + [exp[posini+1:posend]] + exp[posend+1:])
 
-expressions = [["(", "a", ")", "+", "(", "b", "*", "c", ")"],
-               ["a", "+", "b"],
-               ["(", "a", ")"],
+expressions = ["a".split(),
+               "a + b".split(),
+               "( a )".split(),
+               "( ( a ) )".split(),
+               "( ( ( a ) ) )".split(),
                "( a + b ) + c".split(),
-               "( ( a ) )".split() ]
+               "a + ( b + c )".split()
+                ]
 
 for exp in expressions:
-    print exp
     print ReversePolish(exp)
